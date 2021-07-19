f2f4f5349eb7a2a9ec78ebc81e12ab4e7899ebc2
Filmos <git@filmos.net>
2021-07-19 20:21:17 +0200

    Added display of unused skill points

--- kubejs/server_scripts/skillpoint_counting.js
+++ kubejs/server_scripts/skillpoint_counting.js
@@ -6,6 +6,11 @@ onEvent('server.load', event => {
   server.runCommandSilent("scoreboard objectives add TotalSkillPoints dummy \"Total Skill Points\"")
   server.runCommandSilent("scoreboard objectives add UsedSkillPoints dummy \"Used Skill Points\"")
 })
+onEvent('server.tick', event => {
+  let server = event.getServer()
+  server.runCommandSilent('title @a[scores={SkillPoints=2..}] actionbar ["",{"score":{"name":"*","objective":"SkillPoints"},"color":"#af8cf5"},{"text":" unused skill points","color":"#af8cf5"}]')
+  server.runCommandSilent('title @a[scores={SkillPoints=1}] actionbar ["",{"text":"1 unused skill point","color":"#af8cf5"}]')
+})
 
 onEvent('player.inventory.changed', event => {
   if(event.item.id != "kubejs:skill_point") return
