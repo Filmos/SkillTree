5e789e4b93dc71b9fbb8ed5ff8b812de9e800572
Filmos <git@filmos.net>
2021-07-20 20:21:58 +0200

    Added agility and resistance skills

--- static/kubejs/server_scripts/skillpoint_counting.js
+++ static/kubejs/server_scripts/skillpoint_counting.js
@@ -10,6 +10,9 @@ onEvent('server.tick', event => {
   let server = event.getServer()
   server.runCommandSilent('title @a[scores={SkillPoints=2..}] actionbar ["",{"score":{"name":"*","objective":"SkillPoints"},"color":"#af8cf5"},{"text":" unused skill points","color":"#af8cf5"}]')
   server.runCommandSilent('title @a[scores={SkillPoints=1}] actionbar ["",{"text":"1 unused skill point","color":"#af8cf5"}]')
+  server.runCommandSilent('ftbquests change_progress @a reset 10F0F0F0F0F0F000')
+  server.runCommandSilent('ftbquests change_progress @a[scores={SkillPoints=1..}] complete 10F0F0F0F0F0F001')
+  server.runCommandSilent('ftbquests change_progress @a[scores={SkillPoints=2..}] complete 10F0F0F0F0F0F002')
 })
 
 onEvent('player.inventory.changed', event => {
@@ -17,4 +20,16 @@ onEvent('player.inventory.changed', event => {
   event.player.runCommandSilent("clear @s kubejs:skill_point")
   event.player.runCommandSilent("scoreboard players add @s TotalSkillPoints "+event.item.count)
   event.player.runCommandSilent("scoreboard players add @s SkillPoints "+event.item.count)
+  event.player.runCommandSilent('ftbquests change_progress @s reset 10F0F0F0F0F0F000')
+  event.player.runCommandSilent('ftbquests change_progress @s[scores={SkillPoints=1..}] complete 10F0F0F0F0F0F001')
+  event.player.runCommandSilent('ftbquests change_progress @s[scores={SkillPoints=2..}] complete 10F0F0F0F0F0F002')
 })
+deepSearch = function(object, lookingFor, maxLevel, prefix) {
+  if(maxLevel == 0) return
+  for(key in object) {
+    newPrefix = prefix+"."+key
+    if(!key || !object[key]) continue
+    if((key + " ][ " + object[key]).toLowerCase().indexOf(lookingFor.toLowerCase())!=-1) console.info((newPrefix + " ][ " + object[key]).replace(/\n/g, " "))
+    try {deepSearch(object[key], lookingFor, maxLevel-1, newPrefix)} catch(error) {continue}
+  }
+}
